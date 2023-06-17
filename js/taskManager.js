b9e3ee8bfc1c8cdda170500f212660c647699c8e
Lori Moose <lorimoose@Loris-MacBook-Air.local>
2023-06-17 14:56:47 -0500

    fixed task cards

--- js/taskManager.js
+++ js/taskManager.js
@@ -1,11 +1,11 @@
 function createTaskHtml(newActivity, newPlace, aloneOrGroup, newDate) {
-    const html = `
-    <div id="root" class="card">
+  const html = `
+<div class="card col-4 m-5">
     <div class="card-body">
-    <p class="card-text">Name: ${newActivity}</p>
-    <p class="card-text">Place: ${newPlace}</p>
-    <p class="card-text">With: ${aloneOrGroup}</p>
-    <p class="card-text">Date: ${newDate}</p>
+        <p class="card-text">Name: ${newActivity}</p>
+        <p class="card-text">Place: ${newPlace}</p>
+        <p class="card-text">With: ${aloneOrGroup}</p>
+        <p class="card-text">Date: ${newDate}</p>
     </div>
     <div class="card-footer">
         Status:
@@ -14,39 +14,43 @@ function createTaskHtml(newActivity, newPlace, aloneOrGroup, newDate) {
     </div>
 </div>
     `;
-    return html;
+  return html;
 }
 
 class TaskManager {
-    constructor(currentId = 0) {
-        this.tasks = [];
-        this.currentId = currentId;
+  constructor(currentId = 0) {
+    this.tasks = [];
+    this.currentId = currentId;
+  }
+
+  addTask(newActivity, newPlace, aloneOrGroup, newDate, status = "TODO") {
+    this.currentId++;
+    const taskObj = {
+      id: this.currentId,
+      newActivity,
+      newPlace,
+      aloneOrGroup,
+      newDate,
+      status,
+    };
+    this.tasks.push(taskObj);
+  }
+  render() {
+    const tasksHtmlList = [];
+    for (var i = 0; i < this.tasks.length; i++) {
+      let currentTask = this.tasks[i];
+      console.log(currentTask);
+      let taskHtml = createTaskHtml(
+        currentTask["newActivity"],
+        currentTask["newPlace"],
+        currentTask["aloneOrGroup"],
+        currentTask["newDate"]
+      );
+      tasksHtmlList.push(taskHtml);
     }
 
-    addTask(newActivity, newPlace, aloneOrGroup, newDate, status = 'TODO') {
-        this.currentId++;
-        const taskObj = {
-            id: this.currentId,
-            newActivity,
-            newPlace,
-            aloneOrGroup,
-            newDate,
-            status,
-        };
-        this.tasks.push(taskObj);
-    }
-    render() {
-        const tasksHtmlList = [];
-        for (var i = 0; i < this.tasks.length; i++) {
-            let currentTask = this.tasks[i];
-            console.log(currentTask);
-            let taskHtml = createTaskHtml(currentTask['newActivity'], currentTask['newPlace'], currentTask['aloneOrGroup'], currentTask['newDate']);
-            tasksHtmlList.push(taskHtml);
-        }
-
-
-        const tasksHtml = tasksHtmlList.join('\n');
-        let root = document.getElementById('root');
-        root.innerHTML = tasksHtml;
-    }
+    const tasksHtml = tasksHtmlList.join("\n");
+    let root = document.getElementById("root");
+    root.innerHTML = tasksHtml;
+  }
 }
