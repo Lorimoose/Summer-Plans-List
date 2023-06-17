fd262ab7c242cf285c906a825dff85a61afb79d3
Lori Moose <lorimoose@Loris-Air.lan>
2023-06-16 20:54:12 -0500

    changed background

--- js/index.js
+++ js/index.js
@@ -8,28 +8,24 @@ function validFormFieldInput(event) {
     event.preventDefault();
 
     let newActivity = document.querySelector("#activity").value;
-    console.log(newActivity)
     if (newActivity == "") {
         alert("Please fill out an activity");
         return false;
     }
 
     let newPlace = document.querySelector("#place").value;
-    console.log(newPlace)
     if (newPlace == "") {
         alert("Please pick a place.");
         return false;
     }
 
     let aloneOrGroup = document.querySelector("#with-whom").value;
-    console.log(aloneOrGroup)
     if (aloneOrGroup == "") {
         alert("Please state if you will be doing this activity alone or with others.");
         return false;
     }
 
     let newDate = document.querySelector("#date").value;
-    console.log(newDate)
     if (newDate == "") {
         alert("Please choose a date.");
         return false;
@@ -39,7 +35,7 @@ function validFormFieldInput(event) {
     document.getElementById('with-whom').value = "";
     document.getElementById('date').value = "";
 
-    taskManager.addTask('newActivity', 'newPlace', 'aloneOrGroup', 'newDate');
+    taskManager.addTask(newActivity, newPlace, aloneOrGroup, newDate);
     taskManager.render()
 }
 
